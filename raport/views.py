cecfde9307b1f210fe98e0fe2e8158a9bf56ebcb
Mateusz Lewicki <mateuszalewicki@gmail.com>
2016-03-19 21:20:09 +0100

    summary view has list of all funds

--- raport/views.py
+++ raport/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from django.http import HttpResponse
+from .models import InvestmentFund, DataPoint
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Funds index.")
+    funds_list = InvestmentFund.objects.order_by('name')
+    context = {'funds_list': funds_list}
+    return render(request, 'raport/index.html', context)
 
 
 def fund_details(request, fund_id):
+    # question = get_object_or_404(Question, pk=question_id)
+    # return render(request, 'polls/detail.html', {'question': question})
+
     response = "You're looking at the details of fund %s." % fund_id
     return HttpResponse(response)
