2a2129cc58d584cf1a3dae650d8ad771531557b0
Mateusz Lewicki <mateuszalewicki@gmail.com>
2016-10-22 19:01:21 +0200

    server error 500

--- crawler/scrapy_openlife/spiders/openlife.py
+++ crawler/scrapy_openlife/spiders/openlife.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import datetime
 import re
-from report.models import Policy, PolicyOperation
+from report.models import Policy, PolicyOperation, PolicyOperationDetail
 from collections import defaultdict
 from pprint import pprint as pp
 
@@ -16,8 +16,11 @@ class OpenlifeSpider(scrapy.Spider):
     start_urls = (
         'https://portal.openlife.pl/frontend/login.html',
     )
+    needed_details = set()
 
     with open('/tmp/failed_nometa.txt', 'w'), open('/tmp/failed1.txt', 'w') as f1, open('/tmp/failed2.txt', 'w') as f2, open('/tmp/requested.txt', 'w') as r, open('/tmp/good.txt', 'w') as g:
+        pass
+    with open('/tmp/history.txt', 'w'):
         pass
 
     def parse(self, response):
@@ -53,10 +56,11 @@ class OpenlifeSpider(scrapy.Spider):
         date_from = response.meta['date_from']
         min_date = datetime.datetime.strptime(date_from, "%Y-%m-%d").date()
         max_date = datetime.date.today()  # TODO this should be read from page/meta        max_date = datetime.date.today()  # TODO this should be read from page/meta
-
+        meta = reuse_meta(response)
+        meta['wanted_page'] = 1
         yield scrapy.Request('https://portal.openlife.pl/frontend/secure/accountHistory.html',
                              callback=self.on_account_history,
-                             meta=reuse_meta(response),
+                             meta=meta,
                              dont_filter=True)
         for day in daterange(min_date, max_date):
             yield scrapy.FormRequest.from_response(response,
@@ -98,14 +102,55 @@ class OpenlifeSpider(scrapy.Spider):
             # on policy list: date_from = response.xpath("//div[@class='boxContent_lvl1']//tbody//td")[3].xpath('text()')[0].extract().strip()
         return date
 
+    def pop_details(self, url):
+        with open('/tmp/history.txt', 'a') as fil:
+            fil.write("POP ")
+        url_ext, meta = self.needed_details.pop()
+        # # except KeyError:
+        # #     return
+        url = url.split('&', 1)[0]
+        details_url = "%s&%s" % (url, url_ext)
+        meta = dict(meta)
+        with open('/tmp/history.txt', 'a') as fil:
+            fil.write("POP tries %s (%s)\n" % (details_url, str(meta)))
+        return scrapy.Request(details_url,
+                              callback=self.on_history_details,
+                              meta=meta,
+                              dont_filter=True)
+
+
     def on_account_history(self, response):
-        nextpage = response.xpath("//a[@id='linkNextPage']/@href").extract()
-        if nextpage:
-            nextpage = nextpage[0].strip()
-            nextpage = response.urljoin(nextpage)
-            yield scrapy.Request(nextpage,
+        next_page = response.xpath("//a[@id='linkNextPage']/@href").extract()
+        # self.debug(response)
+        curr_page = int(response.xpath('//a[@class="activePage"]/text()').extract()[0].strip())
+        expected_page = response.meta['wanted_page']
+
+        with open('/tmp/history.txt', 'a') as fil:
+            fil.write("curr %s, expected %s\n" % (curr_page, expected_page))
+        if curr_page != expected_page:
+            # retry with new _flowExecutionKey
+            meta = reuse_meta(response, ['wanted_page'])
+            url = response.url + '&_eventId=gotoPage&page=' + str(expected_page)
+            with open('/tmp/history.txt', 'a') as fil:
+                fil.write("RErequesting next %s (%s)\n" % (str(expected_page), url))
+            yield scrapy.Request(url,
                                  callback=self.on_account_history,
-                                 meta=reuse_meta(response))
+                                 meta=meta,
+                                 dont_filter=True)
+            return
+
+        if next_page:
+            next_page = next_page[0].strip()
+            next_page = response.urljoin(next_page)
+            meta = reuse_meta(response)
+            meta['wanted_page'] = curr_page + 1
+
+            with open('/tmp/history.txt', 'a') as fil:
+                fil.write("requesting next %s (%s)\n" % (str(curr_page + 1), next_page))
+            yield scrapy.Request(next_page,
+                                 callback=self.on_account_history,
+                                 meta=meta,
+                                 dont_filter=True)
 
         last_date = (PolicyOperation.latest_date(response.meta['policy'])
                      or datetime.date(year=2000, month=1, day=1)).strftime("%Y-%m-%d")
@@ -114,31 +159,49 @@ class OpenlifeSpider(scrapy.Spider):
         for entry in entries:
             fields = [e.extract().strip() for e in entry.xpath('td/text()')]
             _, op_id, op_date, op_type, op_amount, _, _, details, _ = fields
-            op_type = op_type.encode('utf-8')
-            op_amount = float(op_amount.replace(',', '.').replace(u'\xa0', ''))
-            item = ScrapyOpenlifeHistoryItem()
-            item['id'] = op_id
-            item['date'] = op_date
-            item['type'] = op_type
-            item['amount'] = op_amount
-            item['policy'] = response.meta['policy']
-            yield item
+
+            operation = PolicyOperation.objects.filter(policy=response.meta['policy'], operation_id=op_id)
+            if not operation:
+                op_type = op_type.encode('utf-8')
+                op_amount = float(op_amount.replace(',', '.').replace(u'\xa0', ''))
+                item = ScrapyOpenlifeHistoryItem()
+                item['id'] = op_id
+                item['date'] = op_date
+                item['type'] = op_type
+                item['amount'] = op_amount
+                item['policy'] = response.meta['policy']
+                yield item
+            else:
+                # operation already in db, if details are also then don't download them
+                operation = operation[0]
+                has_details = operation.policyoperationdetail_set.count() > 0
+                if has_details:
+                    continue  # do not download details
 
             details_url = entry.xpath('td/a/@href')[0].extract()
             details_url = response.urljoin(details_url)
             if '_eventId=details' not in details_url:
                 self.debug(response)
-            meta = reuse_meta(response)
+            # meta = reuse_meta(response)
+            meta = dict()
+            meta['policy'] = response.meta['policy']
+            meta['cookiejar'] = response.meta['cookiejar']
             meta['op_id'] = op_id
-            meta['op_type'] = op_type
+            meta['op_type'] = op_type #.encode('utf-8')
             meta['askedfor'] = details_url
-            with open('/tmp/requested.txt', 'a') as f:
-                f.write(details_url)
-                f.write('\n')
-            yield scrapy.Request(details_url,
-                                 callback=self.on_history_details,
-                                 meta=meta,
-                                 dont_filter=True)
+            # with open('/tmp/requested.txt', 'a') as f:
+            #     f.write(details_url)
+            #     f.write('\n')
+            # yield scrapy.Request(details_url,
+            #                      callback=self.on_history_details,
+            #                      meta=meta,
+            #                      dont_filter=True)
+            self.needed_details.add((details_url.split('&', 1)[1], frozenset(meta.items())))
+        # self.debug(response)
+        url = response.url
+        yield self.pop_details(url)
+        yield self.pop_details(url)
+
 
     @staticmethod
     def moneyparse(money):
@@ -149,70 +212,64 @@ class OpenlifeSpider(scrapy.Spider):
         op_type = response.meta['op_type']
 
         if "Parametry wyszukiwania" in response.body:
+            # retry with new _flowExecutionKey
+            suffix = response.meta['redirect_urls'][0].split('&', 1)[1]
+            url = response.url + '&' + suffix
+            meta = reuse_meta(response, ['op_id', 'op_type'])
+            yield scrapy.Request(url,
+                                 callback=self.on_history_details,
+                                 meta=meta,
+                                 dont_filter=True)
+
             with open('/tmp/failed1.txt', 'a') as f:
-                f.write(str(response.meta['redirect_urls']) + '\n')
+                f.write(str(response.meta['redirect_urls']) + ' -->  ' + url + '\n')
+            return
         else:
             with open('/tmp/good.txt', 'a') as f:
                 f.write(str(response.meta['redirect_urls']) + '\n')
-        if 'http://portal.openlife.pl/frontend/secure/accountHistory.html?_flowId=account_history-flow' in response.meta['redirect_urls']:
-            with open('/tmp/failed2.txt', 'a') as f:
-                f.write(str(response.meta['redirect_urls']) + '\n')
 
-
-        try:
-            if op_type == 'Opłata':
-                entries = response.xpath("//div/div/table/tbody/tr")
-                for entry in entries:
-                    fields = [e.extract().strip() for e in entry.xpath('td/text()|td/nobr/text()')]
-                    _, fund_name, units_amount, unit_price, _, price_date, _, money, _, _ = fields
-                    item = ScrapyOpenlifeHistoryItemDetail()
-                    item['op_id'] = op_id
-                    item['fund_name'] = fund_name
-                    item['money_transfer'] = -OpenlifeSpider.moneyparse(money)
-                    item['policy'] = response.meta['policy']
-                    yield item
-            elif op_type == 'Wpłaty':
-                entries = response.xpath("//div/div/table/tr")
-                for entry in entries:
-                    fields = [e.extract().strip() for e in entry.xpath('td/text()|td/nobr/text()')]
-                    _, fund_name, _, _, _, _, _, money, _, _ = fields
-                    item = ScrapyOpenlifeHistoryItemDetail()
-                    item['op_id'] = op_id
-                    item['fund_name'] = fund_name
-                    item['money_transfer'] = OpenlifeSpider.moneyparse(money)
-                    item['policy'] = response.meta['policy']
-                    yield item
-            elif op_type == 'Przeniesienie':
-                entries = response.xpath("//div/div/table/tr")
-                moneytransfers = defaultdict(float)
-                for entry in entries:
-                    fields = [e.extract().strip() for e in entry.xpath('td/text()|td/nobr/text()')]
-                    fund_name, _, _, _, _, _, money, _, _ = fields
-                    moneytransfers[fund_name] += OpenlifeSpider.moneyparse(money)
-                for fund_name, money in moneytransfers.iteritems():
-                    item = ScrapyOpenlifeHistoryItemDetail()
-                    item['op_id'] = op_id
-                    item['fund_name'] = fund_name
-                    item['money_transfer'] = money
-                    item['policy'] = response.meta['policy']
-                    yield item
-            else:
-                pass
-                # self.debug(response)
-        except Exception as e:
+        OPLATA, WPLATA, PRZENIESIENIE = u'Opłata', u'Wpłaty', u'Przeniesienie'
+        if op_type == OPLATA:
+            entries = response.xpath("//div/div/table/tbody/tr")
+            for entry in entries:
+                fields = [e.extract().strip() for e in entry.xpath('td/text()|td/nobr/text()')]
+                _, fund_name, units_amount, unit_price, _, price_date, _, money, _, _ = fields
+                item = ScrapyOpenlifeHistoryItemDetail()
+                item['op_id'] = op_id
+                item['fund_name'] = fund_name
+                item['money_transfer'] = -OpenlifeSpider.moneyparse(money)
+                item['policy'] = response.meta['policy']
+                yield item
+        elif op_type == WPLATA:
+            entries = response.xpath("//div/div/table/tr")
+            for entry in entries:
+                fields = [e.extract().strip() for e in entry.xpath('td/text()|td/nobr/text()')]
+                _, fund_name, _, _, _, _, _, money, _, _ = fields
+                item = ScrapyOpenlifeHistoryItemDetail()
+                item['op_id'] = op_id
+                item['fund_name'] = fund_name
+                item['money_transfer'] = OpenlifeSpider.moneyparse(money)
+                item['policy'] = response.meta['policy']
+                yield item
+        elif op_type == PRZENIESIENIE:
+            entries = response.xpath("//div/div/table/tr")
+            moneytransfers = defaultdict(float)
+            for entry in entries:
+                fields = [e.extract().strip() for e in entry.xpath('td/text()|td/nobr/text()')]
+                fund_name, _, _, _, _, _, money, _, _ = fields
+                moneytransfers[fund_name] += OpenlifeSpider.moneyparse(money)
+            for fund_name, money in moneytransfers.iteritems():
+                item = ScrapyOpenlifeHistoryItemDetail()
+                item['op_id'] = op_id
+                item['fund_name'] = fund_name
+                item['money_transfer'] = money
+                item['policy'] = response.meta['policy']
+                yield item
+        else:
             pass
-            # meta = response.meta
-            # if 'retries' not in response.meta:
-            #     response.meta['retries'] = 16
-            # if response.meta['retries'] == 0:
-            #     pass
-            #     # self.debug(response)
-            # else:
-            #     response.meta['retries'] -= 1
-            #     yield scrapy.Request(meta['askedfor'],
-            #                          callback=self.on_history_details,
-            #                          meta=meta,
-            #                          dont_filter=True)
+            # self.debug(response)
+        yield self.pop_details(response.url)
+
 
 
     def debug(self, response=None, inspect=False, view=True):
@@ -233,5 +290,6 @@ def daterange(start_date, end_date):
             yield day
 
 
-def reuse_meta(response):
-    return {key: response.meta[key] for key in ['policy', 'cookiejar', 'date_from']}
+def reuse_meta(response, additional_keys=None):
+    keys = ['policy', 'cookiejar', 'date_from'] + (additional_keys or [])
+    return {key: response.meta[key] for key in keys if key in response.meta}
