255b38a32844ccd1dfdc860dfb005d19bc84a09f
Mateusz Lewicki <mateuszalewicki@gmail.com>
2016-11-23 23:32:06 +0100

    export feature

--- report/urls.py
+++ report/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     url(r'^policy/(?P<policy_id>[0-9]+)/$', views.policy_details, name='policy_details'),
     # ex: /report/fund/5/
     url(r'^fund/(?P<fund_id>[0-9]+)/$', views.fund_details, name='fund_details'),
+    # ex: /report/export/
+    url(r'^export/$', views.export, name='export'),
 ]
 
